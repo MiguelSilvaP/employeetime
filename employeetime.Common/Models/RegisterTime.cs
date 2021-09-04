2fdaed3821237e967bc9287dc38e82eb57fd8807
MiguelSilvaP <miguelsilvadev@hotmail.com>
2021-09-04 15:34:43 -0500

    Creation of the proyect.

---
+++ employeetime.Common/Models/RegisterTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace employeetime.Common.Models
+{
+    internal class RegisterTime
+    {
+        public int Employee { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+
+    }
+}
